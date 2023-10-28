65945bcf5b8cb16bdad7dea9589bb8dc8d2a0d09
= <=>
2023-10-28 12:45:17 +0100

    updated live 01

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -2,9 +2,14 @@
 <script src="{{ asset('js/jquery-3.6.4.min.js')}}"></script>
 {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> --}}
 
-            <script src="{{ asset('js/popper.min.js')}}"></script>
+          <script src="{{ asset('js/popper.min.js')}}"></script>
                 <script src="{{ asset('js/bootstrap.min.js')}}"></script>
-                <script src="{{ asset('assets/js/bundle.min.js')}}"></script>
+
+
+
+
+                {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+                {{-- <script src="{{ asset('assets/js/bundle.min.js')}}"></script> --}}
                 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
 
 
@@ -29,6 +34,7 @@
          <script src="{{ asset('assets/js/fecha.min.js') }}"></script>
         <script src="{{ asset('assets/js/hotel-datepicker.min.js') }}"></script>
         <script src="{{ asset('assets/js/main.js')}}"></script>
+         {{-- <script src="{{ asset('assets/js/bundle.min.js')}}"></script> --}}
 
 
 
